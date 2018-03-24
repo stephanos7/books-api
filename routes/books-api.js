936cfe91f3d10c71bf2f088cb6cb562e69474d83
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-24 15:11:59 +0000

    plan api routes: get all books, add new book, get single book, edit existing, delete book

--- routes/books-api.js
+++ routes/books-api.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+// require book model
 const Book = require("../models/book-model");
+
+// get all books
+
+// add new book
+
+// get single book
+
+// edit existing book
+
+// delete book
