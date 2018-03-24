1fb017a0c7e4ab789600d56f9c83541f57f9adb3
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-24 15:17:42 +0000

    add GET-all route, use mongoose's .find method to get all books in db, send a json with the error object if an error occurs and return. Else, send the response as a json with all books

--- routes/books-api.js
+++ routes/books-api.js
@@ -5,6 +5,15 @@ const router = express.Router();
 const Book = require("../models/book-model");
 
 // get all books
+router.get("/books", (req, res, next)=>{
+  Book.find((err, allBooks) => {
+    if(err){
+      res.json(err);
+      return;
+    }
+    res.json(allBooks);
+  });
+});
 
 // add new book
 
@@ -13,3 +22,6 @@ const Book = require("../models/book-model");
 // edit existing book
 
 // delete book
+
+// export router to be used by server in server.js
+module.exports = router;
