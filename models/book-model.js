3ed22da722533a389354d5ea4868d3740929595e
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-24 11:34:50 +0000

    add book-model file. Define a mongoose schema for book with mainTitle, secondaryTitle, authorName, coverImage and summary. Export model.

---
+++ models/book-model.js
@@ -0,0 +1,32 @@
+"use-strict";
+
+const mongoose = require("mongoose");
+
+// define the (mongoose) Schema for the book model
+const bookSchema = new mongoose.Schema({
+  mainTitle : {
+    type: String,
+    required: [true, "The main title is required."]
+  },
+  secondaryTitle : {
+    type: String,
+    required: [true, "The secondary title is required"]
+  },
+  authorName : {
+    type: String, default:"Anonymous",
+    required: [true, "The author is required"]
+  },
+  coverImage: {
+    type: String, defualt: ""
+  },
+  summary: String, default: "No book summary has been provided for this publication"
+});
+
+// use this schema to create a new (mongoose) book model;
+
+const Book = mongoose.model("Book", bookSchema);
+// the first argument is the singular name of the collection our model is for. 
+// mongoose automatically looks for the plural version of this model name - ie. books.
+
+// export the book model
+module.exports = Book;
