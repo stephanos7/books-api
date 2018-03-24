1f8cbf6d7958180427d0b0ee2777fd434dfc5508
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-24 11:07:32 +0000

    finalise db configuration, open connection using mongoose, bind error to console and define a one-off connection-success message

--- configs/db.js
+++ configs/db.js
@@ -1,1 +1,15 @@
+"use strict";
 
+const mongoose = require("mongoose");
+const dbName = "book-store";
+
+//db connection
+mongoose.connect(`mongodb://localhost/${dbName}`);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind("console", "database connection problem: "));
+db.once("open", () => {
+  console.log(`Successfully connected to ${dbName} database`);
+})
+})
