987976a09f18fd323689678f770fd0ce218efe7f
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-24 15:02:39 +0000

    set up server app to to use books-api routes

--- server.js
+++ server.js
@@ -2,5 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose"); 
 
 const db = require("./configs/db");
+const booksAPI = require("./routes/books-api");
 
 const server = express();
+
+server.use("/api", booksAPI);
